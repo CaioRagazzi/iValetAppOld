79f18dc87d510633be4807988c8a000b084ee5db
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2020-09-24 18:23:13 -0300

    Login page screen visual

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import Navigator from './src/navigator/index';
 import AuthProvider from './src/contexts/auth';
+import {ThemeProvider} from 'react-native-elements';
 
 const App = () => {
   return (
     <AuthProvider>
-      <Navigator />
+      <ThemeProvider>
+        <Navigator />
+      </ThemeProvider>
     </AuthProvider>
   );
 };
--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
+import {Button, Input, Icon, Card} from 'react-native-elements';
 import {AuthContext} from '../../contexts/auth';
 
 export default function LoginScreen() {
@@ -11,9 +12,76 @@ export default function LoginScreen() {
   };
 
   return (
-    <View>
-      <Text>Login Screen</Text>
-      <Button title="Logar" onPress={() => handleLogin()} />
+    <View style={styles.mainContainer}>
+      <Card containerStyle={styles.cardContainer}>
+        <Card.Title style={styles.cardTitle}>iValet</Card.Title>
+        <View style={styles.subMainContainer}>
+          <Input
+            placeholder="Email"
+            leftIcon={<Icon name="email" size={24} color="black" />}
+          />
+          <Input
+            placeholder="Password"
+            leftIcon={<Icon name="lock" size={24} color="black" />}
+          />
+          <Button
+            containerStyle={styles.button}
+            title="Login"
+            onPress={() => handleLogin()}
+          />
+          <View style={styles.infoContainer}>
+            <Text style={styles.txtForgotPassword}>Esqueci a senha</Text>
+            <View style={styles.forgotPasswordContainer}>
+              <Text style={styles.txtLogin}>Não possui login?</Text>
+              <Text style={styles.txtUnderline}>Cadastre-se</Text>
+            </View>
+          </View>
+        </View>
+      </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  subMainContainer: {
+    padding: 15,
+  },
+  forgotPasswordContainer: {
+    paddingTop: 16,
+    flexDirection: 'row',
+  },
+  infoContainer: {
+    alignItems: 'center',
+  },
+  txtForgotPassword: {
+    textDecorationLine: 'underline',
+    paddingTop: 10,
+    fontSize: 16,
+  },
+  txtUnderline: {
+    textDecorationLine: 'underline',
+    paddingLeft: 2,
+    fontSize: 16,
+  },
+  cardTitle: {
+    textAlignVertical: 'center',
+    color: 'white',
+    backgroundColor: '#2288dd',
+    fontSize: 26,
+    height: 50,
+  },
+  cardContainer: {
+    padding: 0,
+  },
+  txtLogin: {
+    fontSize: 16,
+  },
+  button: {
+    alignSelf: 'center',
+    width: '50%',
+  },
+});

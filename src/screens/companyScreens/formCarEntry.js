d93020116ff4153b0978c075f97a4eccd656686b
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2020-10-03 12:47:36 -0300

    add transaction

--- src/screens/companyScreens/formCarEntry.js
+++ src/screens/companyScreens/formCarEntry.js
@@ -1,18 +1,45 @@
-import React, {useEffect} from 'react';
+import React, {useState, useContext} from 'react';
 import {View} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
-import {Input} from 'react-native-elements';
+import {Input, Button} from 'react-native-elements';
+import axios from '../../services/axios';
+import {AuthContext} from '../../contexts/auth';
+import IconIonicon from 'react-native-vector-icons/Ionicons';
+import {showError} from '../../components/toast';
 
 export default function FormCarEntryScreen({navigation}) {
-  useEffect(() => {
-    navigation.setParams({title: 'teste'});
-  }, [navigation]);
+  const {companyId} = useContext(AuthContext);
+  const [placa, setPlaca] = useState('');
+
+  const addCar = () => {
+    axios
+      .post('transaction', {
+        placa,
+        companyId,
+      })
+      .then((res) => {
+        navigation.popToTop();
+        navigation.navigate('Entrada');
+      })
+      .catch((err) => {
+        if (err.response.data.errno === 1062) {
+          showError('Carro já se encontra no estacionamento!');
+        }
+      });
+  };
+
   return (
     <View>
       <Input
         label="Placa"
         placeholder="Digite a placa do veículo"
         leftIcon={<Icon name="credit-card" size={24} color="black" />}
+        onChangeText={(value) => setPlaca(value)}
+      />
+      <Button
+        icon={<IconIonicon name="checkbox-outline" size={15} color="green" />}
+        title="Button with icon component"
+        onPress={() => addCar()}
       />
     </View>
   );

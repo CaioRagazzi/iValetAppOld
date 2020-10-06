600e967db9bac9ec21c6606635e0dafc9a4f8e15
Caio Eduardo Ragazzi Gemignani <ca.ragazzi@gmail.com>
2020-10-06 18:17:40 -0300

    lógica de caixa

--- src/screens/companyScreens/home.js
+++ src/screens/companyScreens/home.js
@@ -1,11 +1,66 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useEffect, useContext, useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {Text as TextH, Button} from 'react-native-elements';
 import FloatingActionButton from '../../components/floatingActionButton';
+import axios from '../../services/axios';
+import {AuthContext} from '../../contexts/auth';
 
 export default function HomeScreen({navigation}) {
+  const {companyId} = useContext(AuthContext);
+  const [isCaixaOpened, setIsCaixaOpened] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(`caixa/openedCaixa/${companyId}`)
+      .then((res) => {
+        if (res.data.id) {
+          setIsCaixaOpened(true);
+        }
+      })
+      .catch((err) => {
+        if (err.response.data.message === 'Theres no opened Caixa') {
+          setIsCaixaOpened(false);
+        }
+      });
+  }, [companyId]);
+
+  const openCloseCaixa = () => {
+    if (isCaixaOpened) {
+      axios
+        .post('caixa/closeCaixa', null, {
+          params: {
+            companyId,
+          },
+        })
+        .then((res) => {
+          setIsCaixaOpened(false);
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+        });
+    } else {
+      axios
+        .post('caixa/openCaixa', null, {
+          params: {
+            companyId,
+          },
+        })
+        .then((res) => {
+          setIsCaixaOpened(true);
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+        });
+    }
+  };
+
   return (
     <View style={styles.mainContainer}>
-      <Text> Home Company </Text>
+      <TextH h2> Caixa {isCaixaOpened ? 'Aberto' : 'Fechado'} </TextH>
+      <Button
+        title={isCaixaOpened ? 'Fechar Caixa' : 'Abrir Caixa'}
+        onPress={() => openCloseCaixa()}
+      />
       <FloatingActionButton
         text="Entrada"
         onPress={() => navigation.navigate('FormEntryCar')}
@@ -16,6 +71,7 @@ export default function HomeScreen({navigation}) {
 
 const styles = StyleSheet.create({
   mainContainer: {
+    alignItems: 'center',
     flex: 1,
   },
 });
